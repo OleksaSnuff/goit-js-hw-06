90977e6b9faf85f5bc7cc050c65ea454b6e0ab1b
OleksaSnuff <alex1204park@gmail.com>
2023-12-25 12:21:52 +0200

    upd3.0

--- js/task-2.js
+++ js/task-2.js
@@ -10,9 +10,7 @@ class Storage {
     this.#items.push(newItem);
   }
   removeItem(itemToRemove) {
-    if (this.#items.includes(itemToRemove)) {
-      this.#items.splice(this.#items.indexOf(itemToRemove), 1);
-    }
+    this.#items = this.#items.filter(value => value != itemToRemove);
   }
 }
 const storage = new Storage(['Nanitoids', 'Prolonger', 'Antigravitator']);
